e6d6103e6b27850dce5494977762bf3619357ac8
uzair-zahoor <uzairalam724@gmail.com>
2023-05-24 10:58:38 +0500

    first commit by uzair

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      next-js-app
+      next-js-app <br></br>
+      Our First Blog
+
     </main>
   );
 }
